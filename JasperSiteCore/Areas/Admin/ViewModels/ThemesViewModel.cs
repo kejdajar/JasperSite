1b0b492c07d2acd9d1329c9d6b10ff880ff3ef9e
kejdajar <kejdajar@gmail.com>
2018-01-20 10:18:30 +0100

    Admin - theme page edits

---
+++ JasperSiteCore/Areas/Admin/ViewModels/ThemesViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JasperSiteCore.Areas.Admin.ViewModels
+{
+    public class ThemesViewModel
+    {
+        [Required(ErrorMessage ="Zvolte typ databáze")]
+        public string SelectedDatabase { get; set; }
+
+        [Required(ErrorMessage = "Connection String je povinné pole.")]
+        public string ConnectionString { get; set; }
+    }
+}
