dc62f5dcadf4ad11bf39c0702143cf97c7345adb
kejdajar <kejdajar@gmail.com>
2018-01-27 21:36:42 +0100

    Themes paging changes

---
+++ JasperSiteCore/ViewModels/JasperPagingViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JasperSiteCore.ViewModels
+{
+    public class JasperPagingViewModel
+    {
+        public int DisplayPageNumber { get; set; }
+        public int TotalNumberOfPages { get; set; }
+    }
+}
