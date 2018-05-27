81a5f587d852da61aa2075bee9ade876fb456ae8
kejdajar <kejdajar@gmail.com>
2018-05-27 17:29:45 +0200

    Tiny MCE

---
+++ JasperSiteCore/Areas/Admin/Content/Scripts/tinymceinit.js
@@ -0,0 +1,10 @@
+﻿tinymce.init({
+    selector: '#myTextArea',
+
+    //plugins: "image",   
+   
+    //image_list: [
+    //    { title: 'My image 1', value: 'https://www.tinymce.com/my1.gif' },
+    //    { title: 'My image 2', value: 'http://www.moxiecode.com/my2.gif' }
+    //]
+}) 
