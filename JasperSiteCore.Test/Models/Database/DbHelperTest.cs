3dcb6bcba7330a4c53e5630b4125b1440f0bd062
kejdajar <kejdajar@gmail.com>
2018-06-11 18:17:19 +0200

    First moq unit test working

--- JasperSiteCore.Test/Models/Database/DbHelperTest.cs
+++ JasperSiteCore.Test/Models/Database/DbHelperTest.cs
@@ -12,21 +12,16 @@ using System.Linq;
 
 namespace JasperSiteCore.Test.Models.Database
 {
-    [TestFixture]
-    class DbHelperTest
-    {
-
-        [Test]
-        public void DeleteCategory()
-        {
-            // Arrange
+    #region Tutorial
+    /* 
+     / Arrange
           List<Category> categories = new List<Category>() {
                 new Category(){Id=1,Name="Nezařazeno"},
                  new Category(){Id=2,Name="Programování"},
                   new Category(){Id=3,Name="Zábava"},
             };
             IQueryable<Category> iqueryableList = categories.AsQueryable();
-            var mockSetCategories = new Mock<DbSet<Category>>(); 
+            Mock<DbSet<Category>> mockSetCategories = new Mock<DbSet<Category>>(); 
            
             mockSetCategories.As<IQueryable<Category>>().Setup(e => e.Provider).Returns(iqueryableList.Provider);
             mockSetCategories.As<IQueryable<Category>>().Setup(e => e.Expression).Returns(iqueryableList.Expression);
@@ -36,11 +31,11 @@ namespace JasperSiteCore.Test.Models.Database
 
             Mock<IDatabaseContext> mockContext = new Mock<IDatabaseContext>();         
             mockContext.Setup(e=>e.Categories).Returns(mockSetCategories.Object);
-
-            // Add, remove
-
-            //.Callback<Category>((entity) => categories.Remove(entity));
-            //mockContext.Setup(m => m.Categories.Add(It.IsAny<Category>())).Callback((Category category1) => categories.Add(category1));
+            
+            // Add
+            mockSetCategories.Setup(d => d.Add(It.IsAny<Category>())).Callback<Category>(s => categories.Add(s));
+            mockContext.Object.Categories.Add(new Category() { Name="new category 1"});
+            int count_add = mockContext.Object.Categories.Count();
 
             // Removal
             // Removal specification !
@@ -54,12 +49,76 @@ namespace JasperSiteCore.Test.Models.Database
             int count = mockSetCategories.Object.Count();
 
             // WORKING !!!
-          int count1= mockContext.Object.Categories.Count();
+           int count1= mockContext.Object.Categories.Count();
+
+            DbHelper helper = new DbHelper(mockContext.Object);
+         */
+    #endregion
           
 
-            
+    [TestFixture]
+    class DbHelperTest
+    {
+        /// <summary>
+        /// This method automatically creates DbSet mock objects
+        /// </summary>
+        /// <typeparam name="T">Class to be mocked.</typeparam>
+        /// <param name="elements">Underlying data source.</param>
+        /// <returns></returns>
+        private static Mock<DbSet<T>> CreateDbSetMock<T>(List<T> elements) where T : class
+        {
+            var elementsAsQueryable = elements.AsQueryable();
+            var dbSetMock = new Mock<DbSet<T>>();
 
-            ;
+            dbSetMock.As<IQueryable<T>>().Setup(m => m.Provider).Returns(elementsAsQueryable.Provider);
+            dbSetMock.As<IQueryable<T>>().Setup(m => m.Expression).Returns(elementsAsQueryable.Expression);
+            dbSetMock.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(elementsAsQueryable.ElementType);
+            dbSetMock.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(elementsAsQueryable.GetEnumerator());
+            dbSetMock.Setup(d => d.Add(It.IsAny<T>())).Callback<T>(s => elements.Add(s));
+            dbSetMock.Setup(d => d.Remove(It.IsAny<T>())).Callback<T>(s => elements.Remove(s));
+
+            return dbSetMock;
+        }
+
+        [Test]
+        public void DeleteCategory_IdExists_DeletesOnlyCategoryNotArticles()
+        {            
+            // Arrange categories
+            List<Category> categories = new List<Category>() {
+                new Category(){Id=1,Name="Nezařazeno"},
+                 new Category(){Id=2,Name="Programování"},
+                  new Category(){Id=3,Name="Škola"},
+            };
+            Mock<DbSet<Category>> mockSetCategories = CreateDbSetMock(categories);
+
+            // Arrange articles
+            List<Article> articles = new List<Article>() {
+                new Article(){Id=1,Name="Nezařazeno - článek 1"},
+                 new Article(){Id=2,Name="Programování - článek 1"},
+                  new Article(){Id=3,Name="Škola - článek 1"},
+            };
+            Mock<DbSet<Article>> mockSetArticles = CreateDbSetMock(articles);
+
+            // Arrange dbContext
+            Mock<IDatabaseContext> mockContext = new Mock<IDatabaseContext>();         
+            mockContext.Setup(e=>e.Categories).Returns(mockSetCategories.Object);
+            mockContext.Setup(e => e.Articles).Returns(mockSetArticles.Object);
+
+            
+            // Act - deleting first category
+            DbHelper helper = new DbHelper(mockContext.Object);
+            int idToBeDeleted = 2;
+            helper.DeleteCategory(idToBeDeleted);
+
+            // Assert
+            int expectedNumberOfCategories = 2;
+            int actualNumberOfCategores = mockContext.Object.Categories.Count();
+            Assert.That(actualNumberOfCategores,Is.EqualTo(expectedNumberOfCategories));
+
+            int expectedNumberOfArticles = 3;
+            int actualNumberOfArticles = mockContext.Object.Articles.Count();
+            Assert.That(actualNumberOfArticles, Is.EqualTo(expectedNumberOfArticles));
+
         }
 
         //[Test]
