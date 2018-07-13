e34f0bcda8991189ec3c3cd74a30353f33417ff4
kejdajar <kejdajar@gmail.com>
2018-07-13 14:18:06 +0200

    Deleting article from edit window has now confirmation pop-up

--- JasperSiteCore/Areas/Admin/Content/Scripts/AlertWindow.js
+++ JasperSiteCore/Areas/Admin/Content/Scripts/AlertWindow.js
@@ -1,6 +1,10 @@
-﻿
+﻿/**
+ * 
+ *  AlertWindow.js shows bootstrap modal window with custom header, context, and button event. * 
+ * 
+ */
 
-
+    // Creates modal pop-up with custom content
     function getHtmlWindow(header,body) {
      var window = $(`
 <div id="alertWindow" class="modal fade" role="dialog" style="z-index:99999!important">
@@ -13,10 +17,8 @@
                 <h4 class="modal-title" style="margin-left:20px">${header}</h4>
             </div>
             <div class="modal-body">
-              ${body}
-       
-
-</div>
+              ${body} 
+            </div>
             <div class="modal-footer">
                   <a id="deleteModalButton" title="Odstranit" class="btn btn-jasper btn-jasper-delete"> <i class="fa fa-trash fa-1x" aria-hidden="true"></i> Trvale odstranit  </a>
                 <button type="button" class="btn btn-jasper" data-dismiss="modal" title="Zrušit">Zavřít</button>
@@ -26,38 +28,27 @@
     </div>
 </div>
 
-
 `);
 
-
-       
-
-
-
-       
         return window;
-
     }
 
+    // inserts modal into targetDiv and shows pop-up 
     function appendWindow(header,body,action,targetDiv) {
         var window = getHtmlWindow(header, body);
         window.css({
             "display": "block",
         });
 
-        //$(targetDiv).html(window);
         $(targetDiv).html(window);
-
-
       
         $("#deleteModalButton").click(action);
 
-        $("#alertWindow").modal("toggle");
-        //$('.modal-dialog').draggable({
-        //    handle: ".modal-header"
-        //});
+        toggleWindow();
+       
     }
 
+    // closes or opens modal pop-up
     function toggleWindow() {
         $("#alertWindow").modal("toggle");
     }
