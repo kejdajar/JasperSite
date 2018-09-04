23a51bff25fd6cb83ff04eabb17c3b30b297e5a6
kejdajar <kejdajar@gmail.com>
2018-09-04 09:54:09 +0200

    Frontent improvements

--- JasperSiteCore/webpack.config.js
+++ JasperSiteCore/webpack.config.js
@@ -1,28 +1,13 @@
-﻿//const path = require('path');
-//module.exports = {
-//mode: "production",
-//    entry: "./wwwroot/scripts/main.js",
-//    output: {
-//path: path.resolve(__dirname,"wwwroot/bundles"),
-//filename: "bundle.js"
-
-//    },
-//    module:{
-//        rules:[
-//            {
-//                test:/\.css$/,
-//                use:['style-loader','css-loader']
-//            }
-//       ]
-//    }
-
-//};
-
+﻿
 
 const path = require('path');
 module.exports = {
 mode: "production",
-    entry: { theme: "./wwwroot/scripts/theme.js", admin:"./wwwroot/scripts/admin.js" },
+    entry: {
+        theme: "./wwwroot/scripts/theme.js",
+        admin: "./wwwroot/scripts/admin.js",
+        login: "./wwwroot/scripts/login.js"
+    },
     output: {
 path: path.resolve(__dirname,"wwwroot/bundles"),
 filename: "[name].bundle.js"
@@ -33,9 +18,16 @@ filename: "[name].bundle.js"
             {
                 test:/\.css$/,
                 use:['style-loader','css-loader']
-            }, /* loaders for font-awesome */
+            },
+
+            /* regex for font-awesome */
             { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader" },
             { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader" },
+
+            /* regex for images in css file */
+            {
+                test: /\.(png|jpg)$/, loader: 'url-loader'
+            }
        ]
     }
 
---
+++ JasperSiteCore/wwwroot/scripts/login.js
@@ -0,0 +1,13 @@
+﻿window.$ = window.jQuery = require("../../node_modules/jquery/dist/jquery.js");
+require("../../node_modules/popper.js/dist/umd/popper.js");
+require("../../node_modules/bootstrap/dist/js/bootstrap.js");
+require("../../node_modules/bootstrap/dist/css/bootstrap.css");
+require("../../node_modules/font-awesome/css/font-awesome.css");
+require("../../Areas/Admin/Content/Styles/design.css");
+
+
+require("../../node_modules/jquery-ajax-unobtrusive/jquery.unobtrusive-ajax.js");
+require("../../node_modules/jquery-validation/dist/jquery.validate.js");
+require("../../node_modules/jquery-validation-unobtrusive/dist/jquery.validate.unobtrusive.js");
+
+require("../../Areas/Admin/Content/Styles/login-page.min.css");
