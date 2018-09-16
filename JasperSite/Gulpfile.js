43d526ebfb782cdbe19cacc0dcd02c96d87db795
kejdajar <kejdajar@gmail.com>
2018-09-16 21:10:18 +0200

    Added CSS support for disabled Javascript (= no bundling)

--- JasperSite/Gulpfile.js
+++ JasperSite/Gulpfile.js
@@ -1,7 +1,10 @@
 ﻿const gulp = require('gulp');
 
 gulp.task('default', function () {
-    return gulp.src(['node_modules/tinymce/**/*', 'node_modules/jquery-ui-dist/**/*'], {
+    return gulp.src(['node_modules/tinymce/**/*',
+        'node_modules/jquery-ui-dist/**/*',
+        'node_modules/bootstrap/dist/**/*',
+        'node_modules/font-awesome/**/*'], {
         base: 'node_modules'
     }).pipe(gulp.dest('wwwroot/nodeLib'));
 });
